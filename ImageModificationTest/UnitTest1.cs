e398fbd5499cecc2d197cde63c475df813c21915
ChachaNy <nancy.zappellaz@students.hevs.ch>
2016-10-06 21:19:56 +0200

    Ajout du Pojet Test + Images

---
+++ ImageModificationTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageEdgeDetection;
+using System.IO;
+using System.Reflection;
+
+namespace ImageModificationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+          //  ExtBitmap.SwapFilter();
+
+
+        }
+    }
+}
